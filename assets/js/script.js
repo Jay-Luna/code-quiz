f50b4dd523fd75e703f9d96ad42e37e9632f2afd
Jay Luna <Lunajt@gmail.com>
2023-12-23 23:40:23 -0800

    Add questions and 4 choices to match each of the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,33 @@ var bButton = document.querySelector(".b");
 var cButton = document.querySelector(".c");
 var dButton = document.querySelector(".d");
 
-var questions = ['1?','2?', '3?', '4?', '5?'];
-var answers = ["a" ,"b", "c", "a", "c"];
+var questions = ['What does the typeof operator return for null?',
+    'What is the result of 3 + 2 + "7" in JavaScript?',
+    'Which function is used to parse a string to an integer in JavaScript?',
+    'What does the === operator do in JavaScript?',
+    'Which keyword is used to declare a constant in JavaScript?'];
+var question_choices = ['object', 'null', 'undefined', 'number', 
+'12', '327', '43', '57', 'parseInt()', 'parseFloat()', 'toInteger()', 'stringToNumber()',
+'Checks for value equality without type coercion',
+ 'Checks for value equality with type coercion', 
+ 'Assigns a value to a variable', 
+ 'Compares two values and returns true if they are different',
+ 'let', 'var', 'const', 'final'];
+
+var answers = ["a", "d", "a", "a", "c"];
 var count = 0;
+var questionIndex = 0;
 
 function startQuiz() {
     // "none" hides the firstPag
-    firstPage.style.display = "none"; 
+    firstPage.style.display = "none";
     boxQuestion.style.display = "flex";
     question.innerHTML = questions[0];
+    aButton.innerHTML = question_choices[questionIndex];
+    bButton.innerHTML = question_choices[questionIndex+1];
+    cButton.innerHTML = question_choices[questionIndex+2];
+    dButton.innerHTML = question_choices[questionIndex+3];
+    questionIndex = 4;
 }
 
 function goToFinalScore() {
@@ -29,7 +47,12 @@ function changeQuestion() {
     // hide previous answer
     count++;
     question.innerHTML = questions[count];
-    if(count > questions.length) {
+    aButton.innerHTML = question_choices[questionIndex];
+    bButton.innerHTML = question_choices[questionIndex+1];
+    cButton.innerHTML = question_choices[questionIndex+2];
+    dButton.innerHTML = question_choices[questionIndex+3];
+    questionIndex += 4;
+    if (count > questions.length) {
         goToFinalScore();
     }
 }
@@ -39,7 +62,7 @@ function verifyAnswer(event) {
     event.preventDefault();
     var userInput = event.target.getAttribute("class");
 
-    if(answers[count] === userInput) {
+    if (answers[count] === userInput) {
         console.log("isCorrect");
     } else {
         console.log("isWrong");
