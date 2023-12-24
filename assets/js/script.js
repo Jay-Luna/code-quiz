a9955a3b894988241088029baab800b22bb75bbc
Jay Luna <Lunajt@gmail.com>
2023-12-23 23:14:51 -0800

    Switch questions and check answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,57 @@
 var firstPage = document.querySelector(".firstPage");
-var boxQuestion = document.querySelector(".boxQuestion");
-
 var startButton = document.querySelector(".start-button");
-var question = document.querySelector(".question");
-var firstButton = document.querySelector(".first-button");
 
-var questions = ['2', '3', '4', '5'];
+var boxQuestion = document.querySelector(".boxQuestion");
+var question = document.querySelector(".question");
+
+var aButton = document.querySelector(".a");
+var bButton = document.querySelector(".b");
+var cButton = document.querySelector(".c");
+var dButton = document.querySelector(".d");
+
+var questions = ['1?','2?', '3?', '4?', '5?'];
+var answers = ["a" ,"b", "c", "a", "c"];
 var count = 0;
 
 function startQuiz() {
+    // "none" hides the firstPag
     firstPage.style.display = "none"; 
     boxQuestion.style.display = "flex";
+    question.innerHTML = questions[0];
+}
+
+function goToFinalScore() {
+    console.log("goToFinalScore");
 }
 
 // define the function to change the HTML content
 function changeQuestion() {
-    question.innerHTML = questions[count];
+    // hide previous answer
     count++;
-    if(count == 4) count = 0;
+    question.innerHTML = questions[count];
+    if(count > questions.length) {
+        goToFinalScore();
+    }
 }
 
+// method to verify answer of question
+function verifyAnswer(event) {
+    event.preventDefault();
+    var userInput = event.target.getAttribute("class");
+
+    if(answers[count] === userInput) {
+        console.log("isCorrect");
+    } else {
+        console.log("isWrong");
+    }
+
+    // show answer then change questions
+    changeQuestion();
+}
 
 startButton.addEventListener("click", startQuiz);
-firstButton.addEventListener("click", changeQuestion);
+
+aButton.addEventListener("click", verifyAnswer);
+bButton.addEventListener("click", verifyAnswer);
+cButton.addEventListener("click", verifyAnswer);
+dButton.addEventListener("click", verifyAnswer);
