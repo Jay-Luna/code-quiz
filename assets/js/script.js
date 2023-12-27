9dfb16a8c9cfea1bff32cc074684dad2242a044e
Jay Luna <Lunajt@gmail.com>
2023-12-26 20:49:24 -0800

    store high score into local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,132 +1,200 @@
-var highScoreLink = document.getElementById("high-score-link");
-var firstPage = document.querySelector(".firstPage");
-var startButton = document.querySelector(".start-button");
+var pages = ["firstCard", "questionCard", "finalScoreCard"];
 
-var boxQuestion = document.querySelector(".boxQuestion");
-var question = document.querySelector(".question");
-var displayAnswer = document.querySelector(".displayAnswer");
+// nav elements
+var navBar = document.querySelector("nav");
+var highScoreNavButton = document.getElementById("high-score-nav");
 
-var aButton = document.querySelector(".a");
-var bButton = document.querySelector(".b");
-var cButton = document.querySelector(".c");
-var dButton = document.querySelector(".d");
+// first page elements
+var startButton = document.getElementById("start-button");
 
+// question page elements
+var questionEl = document.getElementById("displayQuestion");
+var answerEl = document.getElementById("displayAnswer");
+var questionChoiceButtons = document.querySelectorAll(".questionChoice");
+var aButton = document.getElementById("a");
+var bButton = document.getElementById("b");
+var cButton = document.getElementById("c");
+var dButton = document.getElementById("d");
+
+// final page elements
+var finalScoreValueEl = document.getElementById("final-score-value");
+var initialsInput = document.querySelector("#initials");
+var submitButton = document.getElementById("submit-button");
+
+// high score page elements
+var highScoreEl = document.getElementById("highScoreCard");
+var scoreListEl = document.getElementById("score-list");
+var backButton = document.getElementById("back-button");
+var clearButton = document.getElementById("clear-button");
+
+// question content elements
 var questions = ['What does the typeof operator return for null?',
     'What is the result of 3 + 2 + "7" in JavaScript?',
     'Which function is used to parse a string to an integer in JavaScript?',
     'What does the === operator do in JavaScript?',
     'Which keyword is used to declare a constant in JavaScript?'];
-var question_choices = ['object', 'null', 'undefined', 'number',
+var choices = ['object', 'null', 'undefined', 'number',
     '12', '327', '43', '57', 'parseInt()', 'parseFloat()', 'toInteger()', 'stringToNumber()',
     'Checks for value equality without type coercion',
     'Checks for value equality with type coercion',
     'Assigns a value to a variable',
     'Compares two values and returns true if they are different',
     'let', 'var', 'const', 'final'];
-
 var answers = ["a", "d", "a", "a", "c"];
-var count = 0;
-var questionIndex = 0;
+var questionIndex, choiceIndex;
 
+// timer elements
 var timerEl = document.getElementById("timer");
 var timeLeft = 75;
 var timeInterval;
 
-var finalScoreEl = document.querySelector(".finalScore");
-var finalScoreValueEl = document.getElementById("final-score-value");
+goToNext(0); // set currentPage to first page on load
 
-var highScorePage = document.querySelector(".highScore");
+// Method to navigate to a page based on index
+function goToNext(currentIndex) {
+    hideAllPages();
+
+    // set & display page
+    localStorage.setItem("currentPage", pages[currentIndex]);
+    document.getElementById(pages[currentIndex]).style.display = "flex";
+}
+
+// Hide all page elements
+function hideAllPages() {
+    for (let i = 0; i < pages.length; i++) {
+        // "none" hides the element
+        document.getElementById(pages[i]).style.display = "none";
+    }
+}
+
+// Starts quiz and timer, & navigate to question page
+function startQuiz() {
+    // set default values
+    questionIndex = 0;
+    choiceIndex = 0;
+
+    startCountdown(); // starts timer
+    setQuestionContent(); // set first question content
+
+    // move to show questions page
+    goToNext(1);
+}
+
+// Method to set question & queston choice text content
+function setQuestionContent() {
+    questionEl.innerHTML = questions[questionIndex];
+    aButton.innerHTML = choices[choiceIndex];
+    bButton.innerHTML = choices[choiceIndex + 1];
+    cButton.innerHTML = choices[choiceIndex + 2];
+    dButton.innerHTML = choices[choiceIndex + 3];
+    choiceIndex += 4; // index is always a factor of 4
+}
 
 // Timer that counts down from 75
-function countdown() {  
+function startCountdown() {
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     timeInterval = setInterval(function () {
-      // As long as the `timeLeft` is greater than 1
-      if (timeLeft > 0) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
-        timerEl.textContent =+ timeLeft;
-        // Decrement `timeLeft` by 1
-        timeLeft--;
-      } else {
-        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
         timerEl.textContent = timeLeft;
-        // Use `clearInterval()` to stop the timer
-        clearInterval(timeInterval);
-        // Call the `displayMessage()` function
-        goToFinalScorePage();
-      }
+        timeLeft--;
+        checkQuizDone();
     }, 1000);
-  }
-
-function startQuiz() {
-    countdown();
-    // "none" hides the firstPage
-    firstPage.style.display = "none";
-    boxQuestion.style.display = "flex";
-    question.innerHTML = questions[0];
-    aButton.innerHTML = question_choices[questionIndex];
-    bButton.innerHTML = question_choices[questionIndex + 1];
-    cButton.innerHTML = question_choices[questionIndex + 2];
-    dButton.innerHTML = question_choices[questionIndex + 3];
-    questionIndex = 4;
 }
 
-// go to high score page  & hide every other page
-function goToHighScorePage() {
-    console.log("goToHighScore");
-    highScorePage.style.display = "flex";
-    firstPage.style.display = "none";
-    boxQuestion.style.display = "none";
-    finalScoreEl.style.display = "none";
-}
+// checks if quiz is done and moves to final score page
+function checkQuizDone() {
+    // if time runs out or if questions are all answered
+    if (timeLeft < 1 || questionIndex == questions.length) {
+        // sets current time & final score
+        timerEl.textContent = timeLeft;
+        finalScoreValueEl.textContent = timeLeft;
 
-// hide question box
-function goToFinalScorePage() {
-    console.log("goToFinalScore");
-    finalScoreValueEl.textContent = timeLeft;
-    boxQuestion.style.display = "none";
-    finalScoreEl.style.display = "flex";
-}
-
-// define the function to change the HTML content
-function changeQuestion() {
-    // hide previous answer
-    count++;
-    question.innerHTML = questions[count];
-    aButton.innerHTML = question_choices[questionIndex];
-    bButton.innerHTML = question_choices[questionIndex + 1];
-    cButton.innerHTML = question_choices[questionIndex + 2];
-    dButton.innerHTML = question_choices[questionIndex + 3];
-    questionIndex += 4;
-    if (count == questions.length) {
-        clearInterval(timeInterval);
-        goToFinalScorePage();
+        clearInterval(timeInterval); // stops timer
+        goToNext(2); // go to final score page
     }
 }
 
-// method to verify answer of question
+// Method to verify answer of question
 function verifyAnswer(event) {
-    event.preventDefault();
-    var userInput = event.target.getAttribute("class");
-
-    if (answers[count] === userInput) {
-        // displayAnswer.innerHTML = "Correct!"
-        console.log("isCorrect");
+    var userInput = event.target.getAttribute("id");
+    if (answers[questionIndex] === userInput) {
+        displayAnswer.innerHTML = "Correct! :)";
     } else {
-        // displayAnswer.innerHTML = "Wrong. :("
-        console.log("isWrong");
+        // deduct 10s for wrong answers
+        displayAnswer.innerHTML = "Wrong! :(";
         timeLeft -= 10;
+        checkQuizDone();
     }
 
-    // show answer then change questions
-    changeQuestion();
+    // change question after 1 sec
+    setTimeout(function () {
+        changeQuestion();
+    }, 1000);
 }
 
+// Method to show next question text content
+function changeQuestion() {
+    displayAnswer.innerHTML = "";
+    questionIndex++; // increase questionIndex by 1
+    setQuestionContent();
+    checkQuizDone();
+}
+
+// save user initals & score to local storage
+function saveInitials(event) {
+    event.preventDefault();
+    var scoreList = JSON.parse(localStorage.getItem("scoreList")) || [];
+    var user = {
+        initials: initialsInput.value.trim(),
+        score: timeLeft
+    };
+    scoreList.push(user);
+
+    // add new submission to local storage
+    localStorage.setItem("scoreList", JSON.stringify(scoreList));
+    initialsInput.value = "";
+
+    timeLeft = 75;
+    timerEl.textContent = timeLeft;
+    goToNext(0); // go back to first page
+}
+
+// Go to high score page & hide every other page
+function goToHighScorePage() {
+    highScoreEl.style.display = "flex";
+    navBar.style.display = "none";
+    hideAllPages();
+
+    var scoreList = JSON.parse(localStorage.getItem("scoreList")) || [];
+    const orderedList = scoreList.sort((a, b) => a.score - b.score).reverse();
+    scoreListEl.innerHTML = orderedList.map(i => `<li>${i.initials} - ${i.score}</li>`).join('');
+}
+
+// Go to high score page to page stored in local storage
+function goToPreviousPage() {
+    highScoreEl.style.display = "none";
+    navBar.style.display = "flex";
+
+    var previousPage = localStorage.getItem("currentPage");
+    document.getElementById(previousPage).style.display = "flex";
+}
+
+// Clear the high score list
+function clearHighScoreList() {
+    localStorage.removeItem("scoreList");
+    goToHighScorePage();
+}
+
+/** Click event listeners */
 startButton.addEventListener("click", startQuiz);
 
-aButton.addEventListener("click", verifyAnswer);
-bButton.addEventListener("click", verifyAnswer);
-cButton.addEventListener("click", verifyAnswer);
-dButton.addEventListener("click", verifyAnswer);
+// loop through each questionChoicec buttons
+// and add a click event listener
+questionChoiceButtons.forEach(function (button) {
+    button.addEventListener("click", verifyAnswer);
+});
 
-highScoreLink.addEventListener("click", goToHighScorePage);
+submitButton.addEventListener("click", saveInitials);
+
+highScoreNavButton.addEventListener("click", goToHighScorePage);
+clearButton.addEventListener("click", clearHighScoreList);
+backButton.addEventListener("click", goToPreviousPage);
